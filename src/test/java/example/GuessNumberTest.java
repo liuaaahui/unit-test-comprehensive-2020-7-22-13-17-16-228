14a768fa5d15f7b1d82273afc6fbb2ea6bbac9a5
penny <925580783@qq.com>
2020-07-23 22:02:55 +0800

    [UnitTest][penny]test: add should_return_0A0B_when_answer_is_1234_given_5678 test

--- src/test/java/example/GuessNumberTest.java
+++ src/test/java/example/GuessNumberTest.java
@@ -25,4 +25,15 @@ public class GuessNumberTest {
         //then
         Assertions.assertEquals("2A2B",actual);
     }
+
+    @Test
+    void should_return_0A4B_when_answer_is_1234_given_5678() {
+        //given
+        String inputNumber = "5678";
+        GuessNumber guessNumber = new GuessNumber("1234");
+        //when
+        String actual = guessNumber.guess(inputNumber);
+        //then
+        Assertions.assertEquals("0A0B",actual);
+    }
 }
