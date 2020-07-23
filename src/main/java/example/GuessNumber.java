e5eae1f9672d7e94296cbb40ecfdde4ae9e2c884
penny <925580783@qq.com>
2020-07-23 22:49:23 +0800

    [UnitTest][penny]feature: implement totally different  position but part same number situation

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -8,33 +8,35 @@ public class GuessNumber {
     }
 
     public String guess(String inputNumber) {
-        if(inputNumber.equals(answer)){
+        if (inputNumber.equals(answer)) {
             return "4A0B";
         }
-        if(totallyDifferent(answer,inputNumber)) return "0A0B";
-        if(sameNumberAndDiffPosition(answer,inputNumber)) return "0A4B";
-        return "2A2B";
+        return createResult(answer, inputNumber);
     }
 
-    private boolean sameNumberAndDiffPosition(String answer, String inputNumber) {
+    private String createResult(String answer, String inputNumber) {
+        int sameNum = sameNumber(answer, inputNumber);
+        int samePos = samePosition(answer, inputNumber);
+        return samePos + "A" + (sameNum-samePos) + "B";
+    }
+
+    private int samePosition(String answer, String inputNumber) {
         int count = 0;
         for (int i = 0; i < answer.length(); i++) {
             if (answer.charAt(i) == inputNumber.charAt(i)) {
-                return false;
+                count++;
             }
+        }
+        return count;
+    }
+
+    private int sameNumber(String answer, String inputNumber) {
+        int count = 0;
+        for (int i = 0; i < answer.length(); i++) {
             if (answer.indexOf(inputNumber.charAt(i)) != -1) {
                 count++;
             }
         }
-        return count == 4;
-    }
-
-    private boolean totallyDifferent(String answer, String inputNumber) {
-        for (int i = 0; i < answer.length(); i++) {
-            if (answer.indexOf(inputNumber.charAt(i)) != -1) {
-                return false;
-            }
-        }
-        return true;
+        return count;
     }
 }
