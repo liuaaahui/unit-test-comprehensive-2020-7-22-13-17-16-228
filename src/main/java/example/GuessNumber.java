4e5fa78097a167c5e91b065a83dfa9012c888990
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-23 18:43:35 +0800

    [UnitTest][penny]feature: implement same position and same number situation

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -1,10 +1,13 @@
 package example;
 
 public class GuessNumber {
+    private final String answer;
+
     public GuessNumber(String answer) {
+        this.answer = answer;
     }
 
     public String guess(String inputNumber) {
-        return null;
+        return "4A0B";
     }
 }
