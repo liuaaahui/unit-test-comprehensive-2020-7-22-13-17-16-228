0ac3e9cd0d1d2fe0cc881db8dbac4b54af5bbf66
penny <925580783@qq.com>
2020-07-23 22:26:55 +0800

    [UnitTest][penny]feature: implement totally different  position but totally same number situation

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -12,7 +12,21 @@ public class GuessNumber {
             return "4A0B";
         }
         if(totallyDifferent(answer,inputNumber)) return "0A0B";
+        if(sameNumberAndDiffPosition(answer,inputNumber)) return "0A4B";
         return "2A2B";
+    }
+
+    private boolean sameNumberAndDiffPosition(String answer, String inputNumber) {
+        int count = 0;
+        for (int i = 0; i < answer.length(); i++) {
+            if (answer.charAt(i) == inputNumber.charAt(i)) {
+                return false;
+            }
+            if (answer.indexOf(inputNumber.charAt(i)) != -1) {
+                count++;
+            }
+        }
+        return count == 4;
     }
 
     private boolean totallyDifferent(String answer, String inputNumber) {
