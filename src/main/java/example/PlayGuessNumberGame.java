abf03d8c8cdfe0f9797ba58ad1f317192e2bb51d
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-24 10:54:13 +0800

    [UnitTest][penny]feature: last push error and push PlayGuessNumberGame class again

--- src/main/java/example/PlayGuessNumberGame.java
+++ src/main/java/example/PlayGuessNumberGame.java
@@ -1,4 +1,18 @@
 package example;
 
+import java.util.Scanner;
+
 public class PlayGuessNumberGame {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        AnswerGenerater answerGenerater = new CreateAnswer();
+        GuessNumber guessNumber = new GuessNumber(answerGenerater);
+        for(int i = 0;i<6;i++){
+            String inputNumber = scanner.next();
+            System.out.println(guessNumber.guess(inputNumber));
+            if(guessNumber.guess(inputNumber) == "4A0B") break;
+        }
+    }
 }
