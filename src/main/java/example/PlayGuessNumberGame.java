8dbf8b85dd5aacc3d404792261c10acb8400ecd6
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-27 22:50:22 +0800

    [UnitTest][penny]refactor: make a String  largeTimes

--- src/main/java/example/PlayGuessNumberGame.java
+++ src/main/java/example/PlayGuessNumberGame.java
@@ -7,12 +7,13 @@ public class PlayGuessNumberGame {
 
     private static final String SPACE_PATTERN = " ";
     private static final String NULL_PATTERN = "";
+    private static final int largeTimes = 6;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         AnswerGenerater answerGenerater = new CreateAnswer();
         GuessNumber guessNumber = new GuessNumber(answerGenerater);
-        for (int i = 0; i < 6; i++) {
+        for (int times = 0; times < largeTimes; times++) {
             String inputNumber = scanner.nextLine().replaceAll(SPACE_PATTERN, NULL_PATTERN);
             System.out.println(guessNumber.guess(inputNumber));
             if (guessNumber.guess(inputNumber) == "4A0B"){
