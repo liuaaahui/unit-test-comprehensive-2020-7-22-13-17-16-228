daf4bfa4368f44c37956f93c13e275adaeed4bd1
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-27 22:46:28 +0800

    [UnitTest][penny]refactor: make a String SPACE_PATTERN to replace " "

--- src/main/java/example/PlayGuessNumberGame.java
+++ src/main/java/example/PlayGuessNumberGame.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class PlayGuessNumberGame {
 
 
+    private static final String SPACE_PATTERN = " ";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         AnswerGenerater answerGenerater = new CreateAnswer();
         GuessNumber guessNumber = new GuessNumber(answerGenerater);
         for (int i = 0; i < 6; i++) {
-            String inputNumber = scanner.nextLine().replaceAll(" ","");
+            String inputNumber = scanner.nextLine().replaceAll(SPACE_PATTERN,"");
             System.out.println(guessNumber.guess(inputNumber));
             if (guessNumber.guess(inputNumber) == "4A0B"){
                 System.out.println("YOU WIN!");
