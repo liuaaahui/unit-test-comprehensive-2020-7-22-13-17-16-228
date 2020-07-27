4c6a08b467f55acd0b1b5c6482a6387f9383d0c0
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-27 22:51:29 +0800

    [UnitTest][penny]refactor: make a String WIN_NUMBER

--- src/main/java/example/PlayGuessNumberGame.java
+++ src/main/java/example/PlayGuessNumberGame.java
@@ -8,6 +8,7 @@ public class PlayGuessNumberGame {
     private static final String SPACE_PATTERN = " ";
     private static final String NULL_PATTERN = "";
     private static final int largeTimes = 6;
+    public static final String WIN_NUMBER = "4A0B";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -16,7 +17,7 @@ public class PlayGuessNumberGame {
         for (int times = 0; times < largeTimes; times++) {
             String inputNumber = scanner.nextLine().replaceAll(SPACE_PATTERN, NULL_PATTERN);
             System.out.println(guessNumber.guess(inputNumber));
-            if (guessNumber.guess(inputNumber) == "4A0B"){
+            if (guessNumber.guess(inputNumber) == WIN_NUMBER){
                 System.out.println("YOU WIN!");
                 break;
             }
