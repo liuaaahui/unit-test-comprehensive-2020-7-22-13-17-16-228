25ccb1e9c90300a82a1d1e13059ca7081ad932ca
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-27 22:47:48 +0800

    [UnitTest][penny]refactor: make a String NULL_PATTERN to replace ""

--- src/main/java/example/PlayGuessNumberGame.java
+++ src/main/java/example/PlayGuessNumberGame.java
@@ -6,13 +6,14 @@ public class PlayGuessNumberGame {
 
 
     private static final String SPACE_PATTERN = " ";
+    private static final String NULL_PATTERN = "";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         AnswerGenerater answerGenerater = new CreateAnswer();
         GuessNumber guessNumber = new GuessNumber(answerGenerater);
         for (int i = 0; i < 6; i++) {
-            String inputNumber = scanner.nextLine().replaceAll(SPACE_PATTERN,"");
+            String inputNumber = scanner.nextLine().replaceAll(SPACE_PATTERN, NULL_PATTERN);
             System.out.println(guessNumber.guess(inputNumber));
             if (guessNumber.guess(inputNumber) == "4A0B"){
                 System.out.println("YOU WIN!");
