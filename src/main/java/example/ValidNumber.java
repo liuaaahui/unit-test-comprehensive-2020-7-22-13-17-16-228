a954d20d045e8f1828d156907c8326098963e45a
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-27 23:18:37 +0800

    [UnitTest][penny]refactor: make a string INPUTNUMBER_LENGTH

--- src/main/java/example/ValidNumber.java
+++ src/main/java/example/ValidNumber.java
@@ -1,8 +1,11 @@
 package example;
 
 public class ValidNumber {
+
+    private static final int INPUTNUMBER_LENGTH = 4;
+
     public Boolean isValid(String inputNumber) {
-        if (isAllNumber(inputNumber) && inputNumber.length() == 4) {
+        if (isAllNumber(inputNumber) && inputNumber.length() == INPUTNUMBER_LENGTH) {
             for (int inputNumberIndex = 0; inputNumberIndex < inputNumber.length(); inputNumberIndex++) {
                 for (int innerInputNumberIndex = 0; innerInputNumberIndex < inputNumber.length(); innerInputNumberIndex++) {
                     if (inputNumber.charAt(inputNumberIndex) == inputNumber.charAt(innerInputNumberIndex)) {
