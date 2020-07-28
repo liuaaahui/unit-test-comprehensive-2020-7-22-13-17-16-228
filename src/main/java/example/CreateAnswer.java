91800def6080a0325101bdc4de22371fdd5c6f90
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-28 10:32:22 +0800

    [UnitTest][penny]refactor: make a string BOUND_NUMBER to replace 10 and replace 4 to LENGTH

--- src/main/java/example/CreateAnswer.java
+++ src/main/java/example/CreateAnswer.java
@@ -3,19 +3,27 @@ package example;
 import java.util.Random;
 
 public class CreateAnswer implements AnswerGenerater {
+
+    private static final int BOUND_NUMBER = 10;
+    private static final int LENGTH = 4;
+
     @Override
     public String generate() {
         Random random = new Random();
         int tag[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         String fourNumber = "";
+        //TODO NAMING
         int temp = 0;
-        while (fourNumber.length() != 4) {
-            temp = random.nextInt(10);
+        while (fourNumber.length() != LENGTH) {
+
+            temp = random.nextInt(BOUND_NUMBER);
+
             if (tag[temp] == 0) {
                 fourNumber += temp;
                 tag[temp] = 1;
             }
         }
+        //TODO DELETE
         System.out.println(fourNumber);
         return fourNumber;
     }
